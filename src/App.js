9012da05a1e10d8eb144b747ac413db60a217aed
MANOJKUMARRAJ <manoj.kumar@techmojo.in>
2022-07-14 12:31:35 +0530

    reactassignment3.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import Users from './users.json';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>User Names</h2>
+    {
+      Users.map( user =>{
+        return(
+          <div className="man" key={user.id}>
+            
+            <li>{user.username}</li>
+          </div>
+        )
+      })
+    }
+     
     </div>
   );
 }
